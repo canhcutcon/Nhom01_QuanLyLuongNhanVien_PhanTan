5bd5cb8b155c5ea1a987c5db7c0cef5640d09a66
Zoe <zoe.vo@emagegroup.com>
2022-08-22 10:38:30 +0700

    [Giang] - Add Project Client

---
+++ Nhom01_QuanLyLuong_PhanTan_Client/src/main/java/UI/FrmTrangChinh.java
@@ -0,0 +1,200 @@
+package UI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.border.EmptyBorder;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Window.Type;
+import java.awt.Dialog.ModalExclusionType;
+
+public class FrmTrangChinh {
+	/**
+	 * Launch the application. Tên: Võ Thị Trà Giang Ngày tạo: 21/08/2022
+	 */
+	private JFrame frmCngTyGsb;
+	private JPanel contentPane;
+	private JDesktopPane desktopPane;
+	private JMenu mnHeThong,mntimKiem, mnDanhMuc,mnXuLy, mnCapNhat;
+	private JMenuItem mnItem_HT_DangNhap, mnItem_HT_DangXuat,
+						mnItem_TK_NhanVien, mnItem_TK_PhongBan,
+						mnItem_XL_TinhLuong, mnItem_XL_ChamCong,
+						mnItem_CN_LuongNV, mnItem_CN_MucPhat, mnItem_CN_ChucVu, mnItem_CN_BangChamCong,
+						mnItem_DM_NhanVien, mnItem_DM_PhongBan, mnItem_DM_PhieuPhat;
+
+	private FrmTrangChu frmTrangChu;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmTrangChinh window = new FrmTrangChinh();
+					window.frmCngTyGsb.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FrmTrangChinh() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmCngTyGsb = new JFrame();
+		frmCngTyGsb.setModalExclusionType(ModalExclusionType.TOOLKIT_EXCLUDE);
+		frmCngTyGsb.setExtendedState(Frame.MAXIMIZED_BOTH);
+		frmCngTyGsb.setResizable(false);
+		frmCngTyGsb.setBounds(0, 0, 1550, 850);
+		frmCngTyGsb.setType(Type.POPUP);
+		frmCngTyGsb.setTitle("Công ty GSB");
+		frmCngTyGsb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCngTyGsb.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 15));
+		frmCngTyGsb.setBackground(new Color(255, 192, 203));
+		frmCngTyGsb.setFocusCycleRoot(true);
+		frmCngTyGsb.setFocusableWindowState(false);
+		frmCngTyGsb.setUndecorated(false);
+
+		// === set Main Panel =================================
+		contentPane = new JPanel();
+		contentPane.setBounds(0, 0, 1550, 850);
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		frmCngTyGsb.setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		// // =====================
+		desktopPane = new JDesktopPane();
+		desktopPane.setBounds(0, 0, 1550, 850);
+		desktopPane.setLayout(null);
+		contentPane.add(desktopPane);
+
+//		Internal Jframe
+//		JInternalFrame internalFrame = new JInternalFrame("New JInternalFrame");
+//		internalFrame.setBounds(0, 0, 1530, 800);
+//		internalFrame.setBorder(new EmptyBorder(0, 0, 0, 0));
+//		desktopPane.add(internalFrame);
+//		internalFrame.setVisible(true);
+//		
+		// === interframe
+		initInternalFrame();
+//		Menu 
+		initMenuItem();
+		
+	}
+
+	private void initInternalFrame() {
+//		========================== Frm Trang Chu ======================
+		frmTrangChu = new FrmTrangChu();
+		frmTrangChu.setBounds(0, 0, 1530, 800);
+		frmTrangChu.setBorder(new EmptyBorder(0, 0, 0, 0));
+		desktopPane.add(frmTrangChu);
+		frmTrangChu.setVisible(true);
+		
+	}
+
+	private void initMenuItem() {
+		// ==============================Menu======================--
+		JPanel pMenu = new JPanel();
+		contentPane.add(pMenu,BorderLayout.NORTH);
+		pMenu.setLayout(new CardLayout(0, 0));
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(UIManager.getColor("window"));
+		pMenu.add(menuBar, "name_417659960722100");
+		 
+//		======== Hệ thống ==============================
+		mnHeThong = createJMenu("Hệ Thống", "HinhAnh\\Icon\\MenuHeThong.png");
+		menuBar.add(mnHeThong);
+		
+//		== menu item ==
+		mnItem_HT_DangNhap = createJMenuItem("Đăng Nhập","HinhAnh/Icon/private.png");
+		mnHeThong.add(mnItem_HT_DangNhap);
+		
+		mnItem_HT_DangXuat = createJMenuItem("Đăng Xuất","HinhAnh/Icon/log-out.png");
+		mnHeThong.add(mnItem_HT_DangXuat);
+		
+		
+		
+//		====== Danh mục 
+		mnDanhMuc = createJMenu("Danh Mục", "HinhAnh/Icon/private.png");
+		menuBar.add(mnDanhMuc);
+		
+//		== menu item ==
+		
+		mnItem_DM_NhanVien = createJMenuItem("Nhân Viên", "HinhAnh/Icon/private.png");
+		mnDanhMuc.add(mnItem_DM_NhanVien);
+		
+		mnItem_DM_PhongBan = createJMenuItem("Phòng Ban", "HinhAnh/Icon/private.png");
+		mnDanhMuc.add(mnItem_DM_PhongBan);
+		
+		mnItem_DM_PhieuPhat = createJMenuItem("Phiếu phạt", "HinhAnh/Icon/private.png");
+		mnDanhMuc.add(mnItem_DM_PhieuPhat);
+		
+		//======== Cập nhật
+		mnCapNhat = createJMenu("Cập Nhật","HinhAnh/Icon/private.png");
+		menuBar.add(mnCapNhat);
+		
+		//== menu item
+		
+		mnItem_CN_LuongNV = createJMenuItem("Lương Nhân Viên", "HinhAnh/Icon/private.png");
+		mnCapNhat.add(mnItem_CN_LuongNV);
+		
+		mnItem_CN_MucPhat = createJMenuItem("Mức Phạt", "HinhAnh/Icon/private.png");
+		mnCapNhat.add(mnItem_CN_MucPhat);
+		
+		mnItem_CN_ChucVu = createJMenuItem("Chức Vụ", "HinhAnh/Icon/private.png");
+		mnCapNhat.add(mnItem_CN_ChucVu);
+		
+		mnItem_CN_BangChamCong = createJMenuItem("Bảng Chấm Công", "HinhAnh/Icon/private.png");
+		mnCapNhat.add(mnItem_CN_BangChamCong);
+		
+//		========= Xử lý ====	
+		mnXuLy = createJMenu("Xử Lý", "HinhAnh/Icon/private.png");
+		menuBar.add(mnXuLy);
+		
+		mnItem_XL_TinhLuong = createJMenuItem("Tính Lương", "HinhAnh/Icon/private.png");
+		mnXuLy.add(mnItem_XL_TinhLuong);
+		
+		mnItem_XL_ChamCong = createJMenuItem("Chấm Công", "HinhAnh/Icon/private.png");
+		mnXuLy.add(mnItem_XL_ChamCong);
+		
+//		=========== Tìm kiếm ============
+		mntimKiem = createJMenu("Tìm Kiếm", "HinhAnh/Icon/private.png");
+		menuBar.add(mntimKiem);
+
+		mnItem_TK_NhanVien = createJMenuItem("Nhân Viên", "HinhAnh/Icon/private.png");
+		mntimKiem.add(mnItem_TK_NhanVien);
+		
+		mnItem_TK_PhongBan = createJMenuItem("Phòng Ban", "HinhAnh/Icon/private.png");
+		mntimKiem.add(mnItem_TK_PhongBan);
+		
+	}
+	
+	private JMenu createJMenu(String name, String image) {
+		JMenu mnTemp = new JMenu(name);
+		mnTemp.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		mnTemp.setIcon(new ImageIcon(image));
+		return mnTemp;
+	}
+	
+	private JMenuItem createJMenuItem(String name, String image) {
+		JMenuItem mnItem = new JMenuItem(name);
+		mnItem.setBackground(new Color(135, 206, 250));
+		mnItem.setIcon(new ImageIcon(image));
+		return mnItem;
+	};
+	
+	private void anTatCa() {
+		frmTrangChu.setVisible(false);
+	}
+}
---
+++ Nhom01_QuanLyLuong_PhanTan_Client/src/main/java/UI/FrmTrangChu.java
@@ -0,0 +1,121 @@
+package UI;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Window.Type;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Rectangle;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import javax.swing.Box;
+
+public class FrmTrangChu extends JInternalFrame {
+	private JPanel centerPanel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmTrangChu frame = new FrmTrangChu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmTrangChu() {
+		setBackground(Color.WHITE);
+		initGUI();
+		setUI();
+	}
+
+	public void initGUI() {
+		setFocusCycleRoot(true);
+		setFocusable(true);
+		setFocusCycleRoot(true);
+		getContentPane().setEnabled(false);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+
+	}
+
+	public void setUI() {
+		JPanel leftPanel = new JPanel();
+		leftPanel.setBackground(Color.WHITE);
+		getContentPane().add(leftPanel, BorderLayout.WEST);
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+		JPanel logoPanel = new JPanel();
+		leftPanel.add(logoPanel);
+		logoPanel.setLayout(new BoxLayout(logoPanel, BoxLayout.Y_AXIS));
+
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setIcon(new ImageIcon("HinhAnh\\Logo\\41973c0e30db4c36924ab8e4a4a9cd33 (1).png"));
+		logoPanel.add(lblLogo);
+
+		JPanel rolePanel = new JPanel();
+		rolePanel.setBackground(Color.WHITE);
+		leftPanel.add(rolePanel);
+		rolePanel.setLayout(new BoxLayout(rolePanel, BoxLayout.Y_AXIS));
+		Component verticalStrut = Box.createVerticalStrut(120);
+		rolePanel.add(verticalStrut);
+
+		JLabel lblRole = new JLabel("");
+		lblRole.setIcon(new ImageIcon("HinhAnh\\Logo\\c5d854c65639429c9a675217663b76df.png"));
+		lblRole.setBackground(Color.WHITE);
+		rolePanel.add(lblRole);
+
+		JPanel infoPanel = new JPanel();
+		infoPanel.setBackground(Color.WHITE);
+		rolePanel.add(infoPanel);
+		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+
+		JLabel tblInfo = new JLabel("Tên nhân viên: ");
+		tblInfo.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		infoPanel.add(tblInfo);
+
+		JLabel txtTenNhanVien = new JLabel("Vo Thi tra Giang");
+		txtTenNhanVien.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		infoPanel.add(txtTenNhanVien);
+
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		rolePanel.add(verticalStrut_1);
+
+		JPanel infoRole = new JPanel();
+		infoRole.setBackground(Color.WHITE);
+		rolePanel.add(infoRole);
+		infoRole.setLayout(new BoxLayout(infoRole, BoxLayout.Y_AXIS));
+
+		JLabel lblChucVu = new JLabel("Chức vụ:");
+		lblChucVu.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		infoRole.add(lblChucVu);
+
+		JLabel txtChucVu = new JLabel("Quản lý");
+		txtChucVu.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		infoRole.add(txtChucVu);
+
+		centerPanel = new JPanel();
+		getContentPane().add(centerPanel, BorderLayout.CENTER);
+		JLabel backgroundLabel = new JLabel("");
+		backgroundLabel.setIcon(new ImageIcon("HinhAnh\\Logo\\befunky_layer.jpg"));
+		centerPanel.add(backgroundLabel);
+		setResizable(true);
+		setBounds(-10, 0, 1530, 800);
+	}
+}
